01fe4ef2b1d74c23f357e6965d0960cd459c915a
vishvajitrao1998 <vishvajitrao1998@gmail.com>
2024-11-30 19:12:18 +0530

    initial commit

---
+++ webcolor/colors/static/colors/js/main.js
@@ -0,0 +1,70 @@
+const uploadZone = document.getElementById('upload-zone');
+const uploadContainer = document.getElementById('upload-container');
+const fileInput = document.getElementById('fileInput');
+const imagePreview = document.getElementById('imagePreview');
+const previewImg = document.getElementById('previewImg');
+// Handle drag and drop events
+uploadZone.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    uploadZone.style.backgroundColor = '#d5e6f1';
+});
+
+uploadZone.addEventListener('dragleave', () => {
+
+    uploadZone.style.backgroundColor = '#ecf0f1';
+});
+
+uploadZone.addEventListener('drop', (e) => {
+    alert()
+    e.preventDefault();
+    uploadZone.style.backgroundColor = '#ecf0f1';
+
+    const file = e.dataTransfer.files[0];
+    if (file && file.type.startsWith('image')) {
+        displayImage(file);
+    }
+});
+
+// Handle file input selection
+fileInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (file && file.type.startsWith('image')) {
+        displayImage(file);
+    }
+});
+
+// Display image preview
+function displayImage(file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        // Code to hide drag and drop section from code and display loaded image
+        uploadContainer.style.display = 'none';
+        // Close
+        previewImg.src = e.target.result;
+        imagePreview.style.display = 'block';
+        imagePreview.style.objectFit = 'contain';
+    };
+    reader.readAsDataURL(file);
+}
+
+
+// Code for copy to clipboard
+
+function copyToClipboard(value) {
+    var textToCopy = document.getElementById(value).innerText; // Get the text to copy
+    console.log(textToCopy);
+
+    // Use the Clipboard API to copy the text to the clipboard
+    navigator.clipboard.writeText(textToCopy).then(function () {
+        // Show the modal if the copy was successful
+        var myModal = new bootstrap.Modal(document.getElementById('copyModal'), {
+            keyboard: false
+        });
+        myModal.show();
+    }).catch(function (error) {
+        alert('Failed to copy text: ' + error);
+    });
+};
+
+
+// close
---
+++ webcolor/webcolor/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for webcolor project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webcolor.settings")
+
+application = get_wsgi_application()
